066f09e9f6c8ad18e7198246b5ee53b833d83cbd
Erfanx <irfansyed479@gmail.com>
2020-12-29 02:32:18 +0600

    Update caffeine

--- src/caffeine/inheritanceApp.java
+++ src/caffeine/inheritanceApp.java
@@ -3,11 +3,28 @@ package caffeine;
 
 
 class Product {
+	
+	// Attributes
 	int id;
 	String name;
 	int price;
+	
+	// Constructors
 	Product(){
 		System.out.println("Product Object Constructed");
+	}
+	
+	// Methods
+	void setProductDetails(int id, String name, int price) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+	
+	void showProductDetails() {
+		System.out.println("Product ID: " + id);
+		System.out.println("Product name: " + name);
+		System.out.println("Product price: " + price);
 	}
 }
 
@@ -16,6 +33,10 @@ public class inheritanceApp {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Product product = new Product();
+		System.out.println("Procuct is: " + product);
+		
 
 	}
 
