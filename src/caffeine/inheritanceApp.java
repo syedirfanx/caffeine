30ac098cbed4d67836fbf7a6bd5c35b29d28c1f2
Erfanx <irfansyed479@gmail.com>
2020-12-29 02:18:38 +0600

    Update caffeine

---
+++ src/caffeine/inheritanceApp.java
@@ -0,0 +1,22 @@
+package caffeine;
+
+
+
+class Product {
+	int id;
+	String name;
+	int price;
+	Product(){
+		System.out.println("Product Object Constructed");
+	}
+}
+
+public class inheritanceApp {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
